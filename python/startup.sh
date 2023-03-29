533d2f238e15c0d6c72146a8e48b72dd83f3ca38
eduargenton <eduardov.argenton@outlook.com>
2023-03-29 17:41:37 -0300

    Gunicorn + Whitenoise + correção startup python

--- python/startup.sh
+++ python/startup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
+sleep 3
 python manage.py makemigrations
 python manage.py migrate
 
-if [ -z "$(python manage.py shell -c 'from django.contrib.auth.models import User; print(User.objects.filter(username="'"$DJANGO_SUPERUSER_USERNAME"'").exists())')" ]; then
+if [ -z "$(python manage.py shell -c 'from accounts.models import User; print(User.objects.filter(username="'"$DJANGO_SUPERUSER_USERNAME"'").exists())')" ]; then
     echo "Creating superuser..."
     python manage.py createsuperuser --noinput --username=$DJANGO_SUPERUSER_USERNAME --email=$DJANGO_SUPERUSER_EMAIL --name=$DJANGO_SUPERUSER_NAME
     echo "Superuser created."
     else
         echo "Superuser already exists."
 fi
-
-python manage.py runserver 0.0.0.0:8000
+gunicorn lpn.wsgi:application -b 0.0.0.0:8000
