dbb910f09680329a223466ba05fc048d05688748
eduargenton <eduardov.argenton@outlook.com>
2023-04-06 10:05:25 -0300

    correção criação superuser

--- python/startup.sh
+++ python/startup.sh
@@ -3,12 +3,6 @@ sleep 7
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
+python manage.py createsuperuser --noinput --username=$DJANGO_SUPERUSER_USERNAME --email=$DJANGO_SUPERUSER_EMAIL --name=$DJANGO_SUPERUSER_NAME
 
-if [ -z "$(python manage.py shell -c 'from accounts.models import User; print(User.objects.filter(username="'"$DJANGO_SUPERUSER_USERNAME"'").exists())')" ]; then
-    echo "Creating superuser..."
-    python manage.py createsuperuser --noinput --username=$DJANGO_SUPERUSER_USERNAME --email=$DJANGO_SUPERUSER_EMAIL --name=$DJANGO_SUPERUSER_NAME
-    echo "Superuser created."
-    else
-        echo "Superuser already exists."
-fi
 gunicorn lpn.wsgi:application -b 0.0.0.0:8000
